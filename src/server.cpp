c16384ef466102b60152eee94d9aabb2cd618f4e
SeongHyeon Kwon <heun0180@gmail.com>
2026-09-02 01:42:02 +0900

    Listen for client connections

--- src/server.cpp
+++ src/server.cpp
@@ -5,6 +5,8 @@
 
 int main()
 {
+
+    // tcp 통신에 사용할 소켓 생성
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     if(serverSocket == -1)
@@ -13,12 +15,18 @@ int main()
         return 1;
     }
 
+    std::cout << "Socket created:" << serverSocket << std::endl;
+    
+
+    //서버의 주소와 포트 설정
     sockaddr_in ServerAddress{};
 
     ServerAddress.sin_family = AF_INET;
     ServerAddress.sin_port = htons(7777);
     ServerAddress.sin_addr.s_addr = INADDR_ANY;
 
+
+    // 소켓을 7777 포트에 연결
     int result = bind(
         serverSocket,
         reinterpret_cast<sockaddr*>(&ServerAddress),
@@ -32,9 +40,21 @@ int main()
         return 1;
     }
 
-
     std::cout << "Bind success:port 7777 " << std::endl;
 
+    //클라이언트 접속 대기 준비
+    int listenresult = listen(serverSocket, 5);
+
+    if (listenresult == -1)
+    {
+        std::cout << "Listen failed: " << std::strerror(errno) << std::endl;
+        close(serverSocket);
+        return 1;
+    }
+
+    std::cout << "Server is listening on port 7777 " << std::endl;
+
+    // 서버 소켓 종료
     close(serverSocket);
 
     return 0;
