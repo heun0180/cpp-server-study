7c582c7e83bdac449ab497c0d89c92e70e759168
SeongHyeon Kwon <heun0180@gmail.com>
2026-09-02 01:28:58 +0900

    Bind server socket to port 7777

--- src/server.cpp
+++ src/server.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
 
-int main(){
+int main()
+{
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     if(serverSocket == -1)
@@ -11,7 +13,27 @@ int main(){
         return 1;
     }
 
-    std::cout << "Socket created: " << serverSocket << std::endl;
+    sockaddr_in ServerAddress{};
+
+    ServerAddress.sin_family = AF_INET;
+    ServerAddress.sin_port = htons(7777);
+    ServerAddress.sin_addr.s_addr = INADDR_ANY;
+
+    int result = bind(
+        serverSocket,
+        reinterpret_cast<sockaddr*>(&ServerAddress),
+        sizeof(ServerAddress)
+    );
+
+    if (result == -1)
+    {
+        std::cout << "Bind failed" << std::endl;
+        close(serverSocket);
+        return 1;
+    }
+
+
+    std::cout << "Bind success:port 7777 " << std::endl;
 
     close(serverSocket);
 
